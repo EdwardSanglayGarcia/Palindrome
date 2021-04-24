20eb80156eac64a214017fe474106130ec0af731
unknown <edward_12_1998@yahoo.com>
2021-04-24 20:31:12 +0800

    added files

---
+++ Palindrome/Palindrome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    public class Palindrome
+    {
+        private string userInput;
+
+        public string UserInput 
+        {
+            get  { return userInput; }
+            set  { userInput = value; }
+        }
+
+        public string CheckPalindrome()
+        {
+            string returnValue = string.Empty;
+            char[] reversedUserInput = userInput.ToCharArray();
+            Array.Reverse(reversedUserInput);
+
+            if (userInput == new string(reversedUserInput))
+            {
+                returnValue = "Accepted";
+            }
+            else
+            {
+                returnValue = "Not Accepted";
+
+            }
+
+            return returnValue;
+        }
+    }
+}
